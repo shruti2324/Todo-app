f75761783b5381e7ff9fefa4fb9f799d48ee6e62
shruti2324 <95524435+shruti2324@users.noreply.github.com>
2022-07-07 15:28:30 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBSkkvKpfyQ5csYjDDKpqdyj574-htxAns",
+    authDomain: "to-do-list-app-adc5a.firebaseapp.com",
+    projectId: "to-do-list-app-adc5a",
+    storageBucket: "to-do-list-app-adc5a.appspot.com",
+    messagingSenderId: "623815873554",
+    appId: "1:623815873554:web:ad994a2e7f9625c22f74cb",
+    measurementId: "G-7YWVXSKE2R"
+  };
+  firebase.initializeApp(firebaseConfig);
+export default firebase;
